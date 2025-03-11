21107983f507f063ac90bfc63d67e61881c6c744
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-03-11 21:42:29 +0100

    feat: Added a code 500 error for server-side issues. Will need to be added later to the available routes

---
+++ 500.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.php'; ?>
+    <div class="container">
+        <div class="card">
+            <h1>Une erreur s'est produite.</h1>
+            <p>Nous sommes désolés, une erreur s'est produite lors de votre demande.</p>
+            <a href="/" class="btn">Retourner à l'accueil</a>
+        </div>
+    </div>
+<?php include 'includes/footer.php'; ?>
