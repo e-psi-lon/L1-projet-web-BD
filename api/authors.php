52a322ea08d8964417be23b27a99ce8d5b1d038b
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-04-19 21:33:50 +0200

    feat(API): Improve author and book API routes with additional attributes and better filter

--- api/authors.php
+++ api/authors.php
@@ -1,16 +1,49 @@
 <?php
 require_once 'includes/utils.php';
-header('Content-Type: application/json');
 
+// Get database connection
 $db = getDbConnection();
 
-$query = "SELECT * FROM authors ORDER BY name";
+$attributes = [
+    'author_id' => 'id',
+    'name' => 'name',
+    'url_name' => 'url_name',
+    'birth_year' => 'birth_year',
+    'death_year' => 'death_year',
+    'biography' => 'biography',
+];
+
+
+
+
+// If a query parameter is provided, filter the books based on all the attributes
+$query = "SELECT * FROM authors";
+$conditions = [];
+$params = [];
+foreach ($attributes as $dbField => $paramName) {
+    if (!empty($_GET[$paramName])) {
+        $conditions[] = "$dbField = :$dbField";
+        $params[":$dbField"] = $_GET[$paramName];
+    }
+}
+if (!empty($conditions)) {
+    $query .= " WHERE " . implode(" AND ", $conditions);
+}
 $stmt = $db->prepare($query);
-$stmt->execute();
-
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-$stmt = null;
+$stmt->execute($params);
+$books = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Include a book_count and a book array with the url_title and title
+foreach ($books as &$book) {
+    $book['book_count'] = 0;
+    $stmt = $db->prepare("SELECT COUNT(*) FROM books WHERE author_id = :author_id");
+    $stmt->bindParam(':book_id', $book['book_id']);
+    $stmt->execute();
+    $book['book_count'] = $stmt->fetchColumn();
+    $stmt = $db->prepare("SELECT url_title, title FROM books WHERE author_id = :author_id");
+    $stmt->bindParam(':author_id', $book['author_id']);
+    $stmt->execute();
+    $book['books'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+// Close the database connection
 $db = null;
-
-echo json_encode($result);
+echo json_encode($books, JSON_PRETTY_PRINT);
--- api/books.php
+++ api/books.php
@@ -31,13 +31,17 @@ if (!empty($conditions)) {
 $stmt = $db->prepare($query);
 $stmt->execute($params);
 $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// Include a chapter_count for each book
+// Include a chapter_count for each book and a chapter array with the chapter_number and title
 foreach ($books as &$book) {
     $book['chapter_count'] = 0;
     $stmt = $db->prepare("SELECT COUNT(*) FROM chapters WHERE book_id = :book_id");
     $stmt->bindParam(':book_id', $book['book_id']);
     $stmt->execute();
     $book['chapter_count'] = $stmt->fetchColumn();
+    $stmt = $db->prepare("SELECT chapter_number, title FROM chapters WHERE book_id = :book_id");
+    $stmt->bindParam(':book_id', $book['book_id']);
+    $stmt->execute();
+    $book['chapters'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 // Close the database connection
 $db = null;
