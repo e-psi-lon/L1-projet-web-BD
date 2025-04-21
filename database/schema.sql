c39f1ccda82e5ea717b1206d2e0d1f301de639a7
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-04-21 15:10:51 +0200

    feat(users): Add users into the database

--- database/schema.sql
+++ database/schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE chapters (
 -- Users' table
 CREATE TABLE users (
     user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
