cfe56a9f47eae0f70fdfeb29dac0532043979123
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-04-22 00:06:58 +0200

    feat(database): Add authors, books and chapters (and suggestions for them) to the database

--- database/books.sql
+++ database/books.sql
@@ -2,4 +2,10 @@ INSERT INTO books (author_id, title, url_title, publication_year, description) V
 (1, 'Les Métamorphoses', 'les-metamorphoses', 150, 'Roman en prose, rappelant un voyage spirituel mais dans un trait comique'),
 (1, 'Apologie', 'apologie', 160, 'Plaidoirie faite par Apulée après avoir été accusé de sorcellerie'),
 (34, 'Les Amours', 'les-amours', -16, 'Recueil d''élégies en trois livres où Ovide chante sa passion fictive pour une femme nommée Corinne'),
+(40, 'Lettres', 'lettres', 100, 'Recueil de lettres écrites par Pline le Jeune à des amis et à des personnalités de son temps'),
 (34, 'Les Métamorphoses', 'les-metamorphoses', 8, 'Poème épique en quinze livres relatant l''histoire du monde depuis sa création à travers des récits de métamorphoses mythologiques');
+
+
+INSERT INTO book_suggestions (suggestion_id, author_id, title, url_title, publication_year, description) VALUES
+    (4, 28, 'De Rerum Natura', 'de-rerum-natura', -55, 'Poème philosophique de Lucrèce sur la nature et l’épicurisme'),
+    (7, 34, 'Les Métamorphoses', 'les-metamorphoses', 8, 'Poème épique en quinze livres relatant l''histoire du monde depuis sa création à travers des récits de métamorphoses mythologiques')
---
+++ database/suggestions.sql
@@ -0,0 +1,12 @@
+INSERT INTO suggestions (user_id, suggestion_type, status, admin_notes, reviewed_by) VALUES
+(1, 'author', 'pending', '', NULL),
+(4, 'book', 'pending', '', NULL),
+(5, 'author', 'approved', '', 1),
+(9, 'chapter', 'approved', '', 1),
+(8, 'author', 'reviewed', 'Veuillez vérifier la date de décès', 1),
+(19, 'chapter', 'pending', '', NULL),
+(3, 'book', 'approved', '', 1),
+(2, 'author', 'pending', '', NULL),
+(7, 'chapter', 'reviewed', 'Veuillez vérifier l''oeuvre et l''auteur, il s''agit d''un extrait de "De Rerum Natura"', 1),
+(15, 'author', 'pending', '', NULL),
+(20, 'author', 'rejected', 'Victor Hugo n est pas un auteur latin mais un auteur français', 1);
